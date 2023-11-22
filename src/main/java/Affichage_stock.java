401aecca64bf124b0c1bbdb0177dfbc909b540c3
dillyjulien <dillyjulien@icloud.com>
2023-11-22 18:41:45 +0100

    Début script Connection

--- src/main/java/Affichage_stock.java
+++ src/main/java/Affichage_stock.java
@@ -1,4 +1,22 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class Affichage_stock {
 
+    public static void main(String[] args){
+        String mongoUri = "mongodb://localhost:27017";
+        MongoClient mongoClient = MongoClients.create(mongoUri);
+        MongoDatabase database = mongoClient.getDatabase("projetNoSQL");
+        MongoCollection<Document> collection = database.getCollection("Maillot");
+
+        for (Document document : collection.find()) {
+            System.out.println(document.toJson());
+        }
+
+        mongoClient.close();
+    }
 
 }
--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -1,2 +1,20 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class Connection {
+    public static void connect(){
+        String mongoUri = "mongodb://localhost:27017";
+        MongoClient mongoClient = MongoClients.create(mongoUri);
+        MongoDatabase database = mongoClient.getDatabase("projetNoSQL");
+        MongoCollection<Document> collection = database.getCollection("Article");
+
+        for (Document document : collection.find()) {
+            System.out.println(document.toJson());
+        }
+
+        mongoClient.close();
+    }
 }
